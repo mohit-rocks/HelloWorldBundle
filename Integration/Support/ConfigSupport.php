9385c574d5fbd5b7425e083382d7e2e2b1fdb9e6
Rahul Shinde <shinde.r.a@gmail.com>
2020-05-15 15:45:47 +0530

    Correction in use statement

--- Integration/Support/ConfigSupport.php
+++ Integration/Support/ConfigSupport.php
@@ -6,7 +6,7 @@ use Mautic\IntegrationsBundle\Integration\DefaultConfigFormTrait;
 use Mautic\IntegrationsBundle\Integration\Interfaces\ConfigFormAuthInterface;
 use Mautic\IntegrationsBundle\Integration\Interfaces\ConfigFormInterface;
 use MauticPlugin\HelloWorldBundle\Form\Type\ConfigAuthType;
-use MauticPlugin\HelloWorldBundle\Integration\ZoomWebinarsIntegration;
+use MauticPlugin\HelloWorldBundle\Integration\HelloWorldIntegration;
 
 class ConfigSupport extends HelloWorldIntegration implements ConfigFormInterface, ConfigFormAuthInterface
 {
